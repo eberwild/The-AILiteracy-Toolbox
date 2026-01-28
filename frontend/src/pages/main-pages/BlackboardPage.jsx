931888bbc958bd3339a467c7005e52818119fd2b
Kevin <kevinflotow@arcor.de>
2026-01-28 12:11:51 +0100

    intro text for blackboard and tools page

--- frontend/src/pages/main-pages/BlackboardPage.jsx
+++ frontend/src/pages/main-pages/BlackboardPage.jsx
@@ -12,7 +12,7 @@ function BlackboardPage() {
                 <h2>The Blackboard</h2>
                 <div className="blackboard-spacer"></div>
                 <p className="blackboard-intro">
-                    Each user can create <strong>one</strong> post on this page. Once published, it becomes visible to all other users.
+                     Each user can create <span className="one">one</span> post on this page. Once published, it becomes visible to all other users.
                     Other users can then request collaboration on the post, allowing for interaction and teamwork around shared ideas.
                 </p>
             </div>
--- frontend/src/pages/main-pages/ToolsPage.jsx
+++ frontend/src/pages/main-pages/ToolsPage.jsx
@@ -12,8 +12,13 @@ function ToolsPage() {
                 <h2>AI Literacy Tools</h2>
                 <div className="tools-spacer"></div>
                 <p className="tools-intro">
-                    Each user can create <strong>one</strong> post on this page. Once published, it becomes visible to all other users.
-                    Other users can then request collaboration on the post, allowing for interaction and teamwork around shared ideas.
+                    This toolbox offers a collection of interactive games and tools designed to help you
+                    build AI literacy in a practical and engaging way. By playing, experimenting, and exploring,
+                    you can better understand how AI works, how it is used, and how to interact with it critically and responsibly.
+                    <br/>
+                    At the bottom of the page, registered users can submit their own tools or games to the toolbox. 
+                    After a review process, approved submissions will be made visible to all users.
+                    You can play and explore the tools without an account but contributing to the toolbox requires registration.
                 </p>
             </div>
 
