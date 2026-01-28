294bd47e23665df24a16c7baeaca767d522486f8
Kevin <kevinflotow@arcor.de>
2026-01-28 14:47:34 +0100

    first frontend implementation for the blackboardpage

--- frontend/src/pages/main-pages/BlackboardPage.jsx
+++ frontend/src/pages/main-pages/BlackboardPage.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import MainHeader from "../../components/MainHeader";
 import '../../styles/pages/Blackboard.css';
 
 function BlackboardPage() {
+
+    const [ entryText , setEntryText ] = useState('')
 
     return(
 
@@ -12,18 +15,78 @@ function BlackboardPage() {
                 <h2>The Blackboard</h2>
                 <div className="blackboard-spacer"></div>
                 <p className="blackboard-intro">
-                     Each user can create <span className="one">one</span> post on this page. Once published, it becomes visible to all other users.
+                    Each user can create <span className="one">one</span> post on this page. Once published, it becomes visible to all other users.
                     Other users can then request collaboration on the post, allowing for interaction and teamwork around shared ideas.
+                    <br/>
+                    You need to be logged in to create a new post on the Blackboard.
                 </p>
             </div>
 
             <div className="blackboard">
 
                 <div className="blackboard-entry">
+                    <div className="user-name">
+                        eberwild
+                    </div>
+
+                    <div className="user-text">
+                        Hey all , im looking for someone to work with , i have ver little experience in working with ai 
+                        so i dont want to start a tool alone. Message me ! 
+                    </div>
+
+                    <div className="user-time">
+                        10:33
+                    </div>
+                    
+                </div>
+
+                <div className="blackboard-entry">
+                    <div className="user-name">
+                        eberwild
+                    </div>
+
+                    <div className="user-text">
+                        Hey all , im looking for someone to work with , i have ver little experience in working with ai 
+                        so i dont want to start a tool alone. Message me ! 
+                    </div>
+
+                    <div className="user-time">
+                        10:33
+                    </div>
+                    
+                </div>
+
+                <div className="blackboard-entry">
+                    <div className="user-name">
+                        eberwild
+                    </div>
+
+                    <div className="user-text">
+                        Hey all , im looking for someone to work with , i have ver little experience in working with ai 
+                        so i dont want to start a tool alone. Message me ! 
+                    </div>
+
+                    <div className="user-time">
+                        10:33
+                    </div>
                     
                 </div>
 
             </div>
+
+            <div className="blackboard-input-section">
+                <h2 className='create-info'>
+                    Create a new entry on the Blackboard here !
+                </h2>
+                <div className='entry-spacer'></div>
+                <p>Email and timestamp will automaticly be added to the entry.</p>
+                    <input type="text"
+                           className='blackboard-input'
+                           value={entryText}
+                           onChange={(event) => {
+                                setEntryText(event.target.value)
+                           }} />
+                </div>
             
         </div>
     )
