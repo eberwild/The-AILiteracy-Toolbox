2bf03435cedb0b929a4a35ed69f4386f032b6cb9
Kevin <kevinflotow@arcor.de>
2026-01-31 23:57:14 +0100

    first design of understandingAI page

--- frontend/src/pages/pillar-pages/UnderstandingAI.jsx
+++ frontend/src/pages/pillar-pages/UnderstandingAI.jsx
@@ -1,5 +1,6 @@
 import MainHeader from "../../components/MainHeader";
 import '../../styles/pillarPages/UnderstandingAI.css';
+import AIFields from '../../assets/AI-fields.png';
 
 function UnderstandingAI(){
 
@@ -15,6 +16,38 @@ function UnderstandingAI(){
                     Understanding AI refers to the ability to grasp what artificial intelligence is, how it works at a basic level,
                     what it can and cannot do, and how AI systems are created, trained, and used in real-world contexts.
                 </p>
+            </div>
+
+            <div className="understanding-info">
+                <p className="understanding-text">
+                    Artificial Intelligence is often talked about as if it were one single thing.
+                    In reality, AI is a broad field made up of many different disciplines, methods,
+                    and technologies that work together to enable machines to perform tasks that normally require human intelligence.
+                </p>
+                <div className="understanding-fields">
+                    <img src={AIFields} 
+                        alt="fields-of_AI" 
+                        className="fields-img"
+                    />
+
+                <p className="fields-info">
+                    <strong>Machine Learning</strong> – Systems that learn patterns from data instead of being explicitly programmed.
+                    <br/>
+                    <br/>
+                    <strong>Neural Networks</strong> – Models inspired by the human brain, used to recognize complex patterns.
+                    <br/>
+                    <br/>
+                    <strong>Data & Statistics</strong> – The foundation that allows AI systems to learn, predict, and improve.
+                    <br/>
+                    <br/>
+                    <strong>Natural Language Processing</strong> – Enables machines to understand and generate human language.
+                    <br/>
+                    <br/>
+                    <strong>Computer Vision</strong> – Allows AI to interpret images and videos.
+                </p>
+   
+                </div>
+
             </div>
         </div>
     )
