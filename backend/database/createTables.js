5efe6c327a40db93c96b14ffca90eb1b2682a520
Kevin <kevinflotow@arcor.de>
2026-02-02 15:08:54 +0100

    added blackboardTable and toolTable to createTable function

--- backend/database/createTables.js
+++ backend/database/createTables.js
@@ -1,18 +1,46 @@
 import database from "./knex.js";
 
-// function to create the table User
+// create user table
 export const createTables = async () => {
   // check if the table already exists
-  const exists = await database.schema.hasTable("users");
-  if (!exists) {
+  const userExists = await database.schema.hasTable("users");
+  if (!userExists) {
     // if not -> create the table
     await database.schema.createTable("users", (table) => {
       table.increments("id").primary(); 
       table.string("email").notNullable().unique(); 
       table.string("password").notNullable(); 
       table.string("role").notNullable().defaultTo("user");
+      table.boolean("verified").notNullable().defaultTo(false);
       table.timestamps(true, true);      // created_at & updated_at
     });
-    console.log("Tabelle 'users' erstellt!");
+    console.log("Table users created.");
+  }
+
+  // create blackboard table
+  const blackboardExists = await database.schema.hasTable("blackboard");
+  if (!blackboardExists) {
+    
+    await database.schema.createTable("blackboard", (table) => {
+      table.increments("id").primary(); 
+      table.string("email").notNullable().unique(); 
+      table.string("text").notNullable();
+      table.timestamps(true, true);      
+    });
+    console.log("Table blackboard created.");
+  }
+
+  // create tools table
+  const toolsExists = await database.schema.hasTable("tools");
+  if (!toolsExists) {
+    
+    await database.schema.createTable("tools", (table) => {
+      table.increments("id").primary(); 
+      table.string("email").notNullable().unique(); 
+      table.string("text").notNullable();
+      table.timestamps(true, true);      
+    });
+    console.log("Table blackboard created.");
   }
 };
+
