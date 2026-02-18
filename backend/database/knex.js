4243de18e297601481b3ebf89c23ff28c0adbfce
Kevin <kevinflotow@arcor.de>
2026-02-18 16:12:20 +0100

    final fixes before docker test

--- backend/database/knex.js
+++ backend/database/knex.js
@@ -1,13 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import knex from "knex";
 import path from "path";
 
-//const DBPath = path.resolve(process.env.DB_PATH);
+
+const DBPath = path.resolve(process.env.DB_PATH);
 
 // configure knex -> sql-data
 const database = knex({
   client: "sqlite3",
   connection: {
-    filename: './data/mydb.sqlite'  // path to db-file
+    filename: DBPath  // path to db-file
   },
   useNullAsDefault: true // no errors for value Null
 });
--- backend/models/blackboardModel.js
+++ backend/models/blackboardModel.js
@@ -1,7 +1,7 @@
 import database from "../database/knex.js";
 
 export const getAllEntries = () => {
-    return database("blackboard").select("*").where({ reviewed: true});
+    return database("blackboard").select("*").where({ reviewed: false});
 }
 
 export const createEntry = (email , message) => {
